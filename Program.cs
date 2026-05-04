6d65d1b860fcf2a7714ddb3fb1f38112f5fb1475
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2026-05-04 04:18:02 +0100

    Added configuration for railway db linking through the DATABASE_URL

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+
 using Microsoft.EntityFrameworkCore;
 using ProductRepositoryPattern.Data;
 using ProductRepositoryPattern.Repositories.Implementations;
 using ProductRepositoryPattern.Repositories.Interfaces;
 using ProductRepositoryPattern.Services.Implementations;
 using ProductRepositoryPattern.Services.Interfaces;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +19,33 @@ builder.Services.AddSwaggerGen();
 //builder.Services.AddDbContext<AppDbContext>(options => 
 //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Helper function to convert Railway's DATABASE_URL to Npsql connection string
+
+string GetConnectionString() 
+{
+    var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    if(!string.IsNullOrEmpty(databaseUrl))
+    {
+        var uri = new Uri(databaseUrl);
+        var userInfo = uri.UserInfo.Split(':');
+        var connectionString = new Npgsql.NpgsqlConnectionStringBuilder
+        {
+            Host = uri.Host,
+            Port = uri.Port > 0 ? uri.Port : 5432,
+            Username = userInfo[0],
+            Database = uri.LocalPath.TrimStart('/'),
+            SslMode = Npgsql.SslMode.Require,
+        }.ToString();
+
+        return connectionString;
+    }
+    return builder.Configuration.GetConnectionString("PsqlConnectionString")!;
+}
+
+var connectionString = GetConnectionString();
+
 builder.Services.AddDbContext<AppDbContext>(options => 
-options.UseNpgsql(builder.Configuration.GetConnectionString("PsqlConnectionString")));
+options.UseNpgsql(connectionString));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddScoped<IProductService, ProductService>();
