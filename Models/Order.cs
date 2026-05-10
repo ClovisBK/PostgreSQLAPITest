b314ba311b68a6ffc61185fb1482783bf7c9a983
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2026-05-10 02:50:02 +0100

    Added the order table

---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductRepositoryPattern.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string OrderId { get; set; } = string.Empty;
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
